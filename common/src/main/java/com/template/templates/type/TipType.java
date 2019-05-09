0361ef0b87d58a92fdef42f71c807dadc4496e47
fenghaiyu <fenghaiyu@fotoable.com>
2019-05-09 15:29:16 +0800

    ~~

---
+++ common/src/main/java/com/template/templates/type/TipType.java
@@ -0,0 +1,18 @@
+package com.template.templates.type;
+
+public class TipType {
+
+    public static final int LevelLess = 1;//等级不足
+
+    public static final int CoinLess = 2;//银币不足
+
+    public static final int GoldLess = 3;//金币不足
+
+
+    public static final int FIRST_ID = 1;//第一个ID
+
+    public static final int LAST_ID = 3;//最后一个ID
+
+    public static final int TYPE_SIZE = 3;//ID数量
+
+}
